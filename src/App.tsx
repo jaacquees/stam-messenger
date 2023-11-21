c3077ca1d2dc35f9ee32748f4cb3e8dc418d804a
jaacquees <jbarouhiel@gmail.com>
2023-11-21 22:42:59 +0400

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@ import * as React from 'react';
 import {AppBar,Box,Toolbar,Typography,Button,Avatar} from '@mui/material';
 import DiscussionList from './DiscussionList';
 import DiscussionDetail from './DiscussionDetail';
+import {User,Message,Discussion} from './Types';
 
+import {me} from './data';
 export default function App() {
   return (
     <Box sx={{ flexGrow: 1 }}>
     <AppBar position="static">
       <Toolbar>
-        <Avatar src="https://gravatar.com/avatar/6767d9badf28afc8d646049ee1112d3b?s=400&d=robohash&r=pg"/>
+        <Avatar src={me.avatarUrl}/>
 
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          News
+          {me.name}
         </Typography>
         <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
-    <Box display="flex" width="100vw" height="90vh" border="1px red solid">
-      <Box width="30vw"><DiscussionList/></Box>
+    <Box display="flex" width="100vw" height="calc(100vh - 64px)" border="3px pink dashed">
+      <Box width="30vw" overflow="hidden" sx={{overflowY:"scroll"}}><DiscussionList/></Box>
       <Box flexGrow="1"  overflow="hidden" sx={{overflowY:"scroll"}}><DiscussionDetail/></Box>
     </Box>
   </Box>
--- src/data.ts
+++ src/data.ts
@@ -2,8 +2,10 @@ import {User,Message,Discussion} from './Types';
 
 const user1:User = {name:"Joe Porcaro",avatarUrl:"https://gravatar.com/avatar/176e27f9bd3b68c235b24bd572286856?s=400&d=robohash&r=x",email:"joe@porcaro.com"}
 
-
 const user2:User = {name:"Stuart Copeland",avatarUrl:"https://gravatar.com/avatar/99ffb243c5df5fcdc97b7d2dd02e4bb8?s=400&d=robohash&r=x",email:"stuart@copeland.com"}
+
+const user3:User = {name:"Phil Collins",avatarUrl:"https://gravatar.com/avatar/6767d9badf28afc8d646049ee1112d3b?s=400&d=robohash&r=pg",email:"phil@collins.com"}
+
 
 const message1:Message = {
     sender: user1,
@@ -18,12 +20,14 @@ const message2:Message = {
 }
 
 const discussion1:Discussion = {
-  members:[user1,user2],
+  members:[user1,user2,user3],
   subject:"guitar strings",
   messages:[message1,message2]
 }
 
-
+export const me:User = user3;
 export const users:User[] = [user1,user2];
+
+
 
 export const discussions:Discussion[] = [discussion1];
